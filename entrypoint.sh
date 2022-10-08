471cf2e6b28d67b54a880ab4173a58c886457c33
Anu Priya <anupriya17@users.noreply.github.com>
2022-10-09 01:45:03 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apk update && apk add --no-cache git && apk add --no-cache openssh
-mkdir -p ~/.ssh && echo $SSH_KEY | base64 -d > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa && ssh-keyscan $MANIFEST_HOST >> ~/.ssh/known_hosts
+mkdir -p ~/.ssh && echo $SSH_CONFIG | base64 -d > ~/.ssh/config && echo $SSH_KEY | base64 -d > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa && ssh-keyscan $MANIFEST_HOST >> ~/.ssh/known_hosts
 rm -rf $MANIFEST_REPO && git clone ssh://git@$MANIFEST_HOST/$MANIFEST_USER/$MANIFEST_REPO.git
 
 cd $MANIFEST_REPO
